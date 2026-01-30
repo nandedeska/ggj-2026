ae4642493f3fddb31fef2ff513236616f95f5dab
PRINZOP <prinz.lim@yahoo.com>
2026-01-31 00:29:29 +0800

    Logo Screen
    Next up: fade in to title

---
+++ scripts/entry.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const menu:PackedScene = preload("res://scenes/mainMenu.tscn")
+
+# Starts up the timer
+func _ready() -> void:
+	print("test")
+	$Timer.connect("timeout",startScreen)
+	$AnimationPlayer.connect("animation_finished",finishedAnim)
+
+func startScreen()->void:
+	$AnimationPlayer.play("bounceIn")
+
+func finishedAnim(animName:StringName)->void:
+	if animName == "bounceIn":
+		get_tree().change_scene_to_packed(menu)
