b5e66bfc12f1128faaa04c348ace909e3fdf07b9
PRINZOP <Prinz.lim@yahoo.com>
2026-01-30 23:41:42 +0800

    UI stuff

--- scripts/play.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-func _input(event)->void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			get_tree().change_scene_to_file("res://scenes/openingCutscene.tscn")
