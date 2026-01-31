4d8f524ee1c8b158c8d5f561d1d55f3f717bfb4f
unknown <miguelvisquera26@gmail.com>
2026-01-31 19:19:10 +0800

    Add ghost

--- scripts/ghost.gd
+++ scripts/ghost.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
 @export var speed := 75
-@export var player: Node2D
+@export var sprite: Sprite2D
 var is_active: bool
 
+func _ready() -> void:
+	Global.player.on_mask_wear.connect(on_player_mask_wear)
+	set_physics_process(false)
+	sprite.visible = false
+
 func _physics_process(delta: float) -> void:
-	var direction := (player.position - position).normalized()
+	var direction := (Global.player.position - position).normalized()
 	velocity = direction * speed
 	
 	move_and_slide()
+	
+func on_player_mask_wear(is_wearing_mask: bool) -> void:
+	set_physics_process(is_wearing_mask)
+	sprite.visible = is_wearing_mask
