4c5419cd374870e3a1aeb5c7741f7e868fe9733f
Magoo <Bautistamanuel248@Gmail.com>
2026-01-31 16:56:05 +0800

    death screen update

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var animation: AnimationPlayer = $DeathTransition/AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("you died")
 	timer.start()
+	animation.play("out")
+	await animation.animation_finished
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	animation.play_backwards("out")
