690cebfff1cbf0fc016525c3097c9ef1baa4d8d0
willarboleda-bananbread <will_arboleda@dlsu.edu.ph>
2026-01-31 20:19:33 +0800

    fuck life

--- scripts/cameraPathFollow.gd
+++ scripts/cameraPathFollow.gd
@@ -3,9 +3,11 @@ extends PathFollow2D
 @export var panSpeed: float = 0.01
 var movementDone: bool = false
 
+signal nextLevel
 
 func _on_right_body_entered(body: Node2D) -> void:
 	while progress_ratio < 0.99 and movementDone == false:
 		progress_ratio += panSpeed
 		await get_tree().create_timer(0.01).timeout
 	movementDone = true
+	nextLevel.emit()
