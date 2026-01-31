b6bf77d1bddac8a844d0b37dc5414ec467e1f730
PRINZOP <prinz.lim@yahoo.com>
2026-02-01 00:12:16 +0800

    NEW MAIN MENU (NOW TRANSITIONS TO LEVEL1)

---
+++ scripts/menuHandler.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# Fades screen out and plays constant menu movement
+func _ready() -> void:
+	$maskTransition.play("openUp")
+	$mainTitle/play.connect("pressed",playCutscene)
+	$mainTitle/credits.connect("pressed",openCredits)
+
+func playCutscene()->void:
+	var scene:PackedScene = load("res://scenes/openingCutscene.tscn")
+	get_tree().change_scene_to_packed(scene)
+
+func openCredits()->void:
+	print("Credits here")
