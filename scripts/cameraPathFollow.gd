b45ae1174af5895f35603128afcde7cc62ea34de
willarboleda-bananbread <will_arboleda@dlsu.edu.ph>
2026-01-31 17:17:26 +0800

    4

--- scripts/cameraPathFollow.gd
+++ scripts/cameraPathFollow.gd
@@ -3,7 +3,6 @@ extends PathFollow2D
 @export var panSpeed: float = 0.01
 var movementDone: bool = false
 
-
 func _on_right_body_entered(body: Node2D) -> void:
 	while progress_ratio < 0.99 and movementDone == false:
 		progress_ratio += panSpeed
