80a67217932d2425c799391f77d6ac8c2174c0f9
PRINZOP <prinz.lim@yahoo.com>
2026-02-01 00:56:38 +0800

    NEW CUTSCENES

--- scripts/opening_cutscene.gd
+++ scripts/opening_cutscene.gd
@@ -1,30 +1,31 @@
 extends Node2D
 
-var cutscenesToPlay = [
+var cutscenes = [
 	"cutscene1",
 	"cutscene2",
 	"cutscene3"
 ]
 
-var currentScene: int = 0
-func _ready() -> void:
-	$Timer.connect("timeout",playCutscenes)
+var currScene:int = 0
 
-func playCutscenes()->void:
-	$fade/fadeAnims.connect("animation_finished",fadeFinish)
-	$cutsceneAnimator.connect("animation_finished",cutsceneFinish)
-	$cutsceneAnimator.play(cutscenesToPlay[currentScene])
-	$fade/fadeAnims.play("fadeIn")
+func _ready() -> void:
+	$Timer.connect("timeout",startScenes)
+
+func startScenes()->void:
+	$fadeItem/fadePlayer.connect("animation_finished",fadeFinish)
+	$fadeItem/fadePlayer.play("fadeIn")
+	$cutscenePlayer.connect("animation_finished",progressCut)
+	$cutscenePlayer.play(cutscenes[currScene])
+
+func progressCut(animName:StringName)->void:
+	$fadeItem/fadePlayer.play("fadeOut")
 
 func fadeFinish(animName:StringName)->void:
 	if animName == "fadeOut":
-		if currentScene != (cutscenesToPlay.size()-1):
-			currentScene += 1
-			$fade/fadeAnims.play("fadeIn")
-			$cutsceneAnimator.play(cutscenesToPlay[currentScene])
+		if currScene < (cutscenes.size()-1):
+			currScene += 1
+			$fadeItem/fadePlayer.play("fadeIn")
+			$cutscenePlayer.play(cutscenes[currScene])
 		else:
 			var scene:PackedScene = load("res://scenes/LEVEL_ONE.tscn")
 			get_tree().change_scene_to_packed(scene)
-
-func cutsceneFinish(animName:StringName)->void:
-	$fade/fadeAnims.play("fadeOut")
