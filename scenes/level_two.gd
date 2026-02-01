a2b8d2ec676eb63107b74c6b04ebbc0a3455e725
willarboleda-bananbread <will_arboleda@dlsu.edu.ph>
2026-02-01 10:50:29 +0800

    transition to three

--- scenes/level_two.gd
+++ scenes/level_two.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 func _on_path_follow_2d_next_level() -> void:
 	Transitioner.transition()
 	await Transitioner.on_transition_finished
-	get_tree().change_scene_to_file("res://scenes/LEVEL_TWO.tscn")
+	get_tree().change_scene_to_file("res://scenes/LEVEL_THREE.tscn")
